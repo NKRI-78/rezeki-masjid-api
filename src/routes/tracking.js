b393e11a53c135551b228e38e0e8502550c14523
reihan agam <reihanagam7@gmail.com>
2026-03-05 11:41:22 +0700

    update: revoke jwt jne tracking

--- src/routes/tracking.js
+++ src/routes/tracking.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const tracking = require('../controllers/tracking');
-const jwt = require('../middlewares/jwt');
 
-router.post('/jne', jwt, tracking.jne);
+router.post('/jne', tracking.jne);
 
 module.exports = router;
